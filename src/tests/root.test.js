b35650ff99d0982ee81caa67c0053a08be20bbf3
Septian Ahmad Fujianto <septianfujianto@outlook.com>
2018-09-29 17:47:50 +0700

    Add more test for root

--- src/tests/root.test.js
+++ src/tests/root.test.js
@@ -10,9 +10,17 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('Root component', () => {
   it('Render Provider and Router', () => {
     const rendered = shallow(<Root />);
-
+    const componentStore = rendered.find(Provider).props().store ;
+    
+    expect(componentStore !== null && typeof componentStore !== 'undefined').toEqual(true)
     expect(rendered.find(Provider).exists()).toEqual(true);
     expect(rendered.find(Router).exists()).toEqual(true);
     expect(rendered.find(Route).length).toEqual(3);
+    expect(rendered.find(Route).at(0).props().path).toEqual("/");
+    expect(rendered.find(Route).at(0).props().exact).toEqual(true);
+    expect(rendered.find(Route).at(1).props().path).toEqual("/nested-fragment");
+    expect(rendered.find(Route).at(1).props().exact).toEqual(true);
+    expect(rendered.find(Route).at(2).props().path).toEqual("/list");
+    expect(rendered.find(Route).at(2).props().exact).toEqual(true);
   });
 })
