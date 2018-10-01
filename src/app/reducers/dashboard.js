93593ba2a980e77745df56bde8e6ed0c13b56a51
Septian Ahmad Fujianto <septianfujianto@outlook.com>
2018-10-01 15:27:53 +0700

    Fix wrong logic on storing posts data

--- src/app/reducers/dashboard.js
+++ src/app/reducers/dashboard.js
@@ -6,15 +6,17 @@ const initialState = {
 }
 
 export default function Dashboard(state = initialState, action) {
-  state = Object.keys(state).length === 0 ? initialState : state;
-  
+  if (Object.keys(state).length === 0) {
+    state = initialState;
+  }
+
   switch (action.type) {
     case SET_NAME:
       const newName = action.payload.name;
       return { ...state, name: newName };
 
     case FETCH_POSTS:
-      const latestPosts = state.posts.concat(action.payload.posts);
+      const latestPosts = action.payload.posts;
       return { ...state, posts: latestPosts };
 
     default:
